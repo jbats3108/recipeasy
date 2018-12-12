a415567a338ddc1e7331d5e44a0f1ccbcdc80618
jbats3108 <jamiebatabyal@gmail.com>
2018-12-12 11:36:24 +0800

    wrote code

--- lib/recipeasy.rb
+++ lib/recipeasy.rb
@@ -1,6 +1,15 @@
 require "recipeasy/version"
+require "net/http"
+require "json"
 
 module Recipeasy
   class Error < StandardError; end
-  # Your code goes here...
+
+  result= Net::HTTP.get("www.edamam.com","/search?type=Feeds")
+
+  recipes = JSON.parse(result)
+
+  puts recipes.first["items"].first["url"]
+
+
 end
